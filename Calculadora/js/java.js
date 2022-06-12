70056f621128a98e703168b5c22a0c395a0742ba
carlos3416B <105512546+carlos3416B@users.noreply.github.com>
2022-06-12 10:12:38 -0500

    Calculadora

---
+++ Calculadora/js/java.js
@@ -0,0 +1,441 @@
+window.onload=function (){
+   return resultado();
+ 
+}
+y=1;
+
+let contador;
+variable=0;
+
+function asignarcero(){
+
+    contador++;
+    
+switch(contador-1){
+    case 1:
+        variable=variable*10;
+    break;
+    case 2:
+        variable=variable*10;
+        break;
+    case 3:
+        variable=variable*10;
+        break;
+        case 4:
+            variable=variable*10;
+        break;
+        case 5 :
+        variable=variable*10;
+        break;
+        case 6 :
+        variable=variable*10;
+        break;
+        case 7 :
+        variable=variable*10;
+        break;
+        case 8 :
+        variable=variable*10;
+        break;
+        
+}
+
+}
+function asignar1(){
+   
+
+contador++;
+switch(contador){
+    case 1:
+        variable=1;
+        salvar1=variable;
+        y=0;
+    break;
+    case 2:
+        variable=11;
+        salvar1=variable;
+        y=0;
+        break;
+    case 3:
+        variable=111;
+        salvar1=variable;
+        y=0;
+        break;
+        case 4:
+            variable=1111;
+            salvar1=variable;
+            y=0;
+        break;
+        case 5 :
+        variable=11111;
+        salvar1=variable;
+        y=0;
+        break;
+        case 6 :
+        variable=111111;
+        salvar1=variable;
+        y=0;
+        break;
+        case 7 :
+        variable=1111111;
+        salvar1=variable;
+        y=0;
+        break;
+        case 8 :
+        variable=11111111;
+        salvar1=variable;
+        y=0;
+        break;
+    
+            
+}
+
+}
+
+
+    
+
+function asignar2(){
+    y=0;
+
+    contador++;
+    switch(contador){
+        case 1:
+            variable=2;
+        break;
+        case 2:
+            variable=22;
+            break;
+        case 3:
+            variable=222;
+            break;
+            case 4:
+                variable=2222;
+            break;
+            case 5 :
+            variable=22222;
+            break;
+            case 6 :
+            variable=222222;
+            break;
+            case 7 :
+            variable=2222222;
+            break;
+            case 8 :
+            variable=22222222;
+            break;
+            
+    }
+    }
+    function asignar3(){
+        y==1
+        contador++;
+        switch(contador){
+            case 1:
+                variable=3;
+            break;
+            case 2:
+                variable=33;
+                break;
+            case 3:
+                variable=333;
+                break;
+                case 4:
+                    variable=3333;
+                break;
+                case 5 :
+                variable=33333;
+                break;
+                case 6 :
+                variable=333333;
+                break;
+                case 7 :
+                variable=3333333;
+                break;
+                case 8 :
+                variable=33333333;
+                break;
+                
+        }
+        }
+        function asignar4(){
+            contador++;
+            switch(contador){
+                case 1:
+                    variable=4;
+                break;
+                case 2:
+                    variable=44;
+                    break;
+                case 3:
+                    variable=444;
+                    break;
+                    case 4:
+                        variable=4444;
+                    break;
+                    case 5 :
+                    variable=44444;
+                    break;
+                    case 6 :
+                    variable=444444;
+                    break;
+                    case 7 :
+                    variable=4444444;
+                    break;
+                    case 8 :
+                    variable=44444444;
+                    break;
+                    
+            }
+            }
+            function asignar5(){
+                contador++;
+                switch(contador){
+                    case 1:
+                        variable=5;
+                    break;
+                    case 2:
+                        variable=55;
+                        break;
+                    case 3:
+                        variable=555;
+                        break;
+                        case 4:
+                            variable=5555;
+                        break;
+                        case 5 :
+                        variable=55555;
+                        break;
+                        case 6 :
+                        variable=555555;
+                        break;
+                        case 7 :
+                        variable=5555555;
+                        break;
+                        case 8 :
+                        variable=55555555;
+                        break;
+                        
+                }
+                }
+                function asignar6(){
+
+                    contador++;
+                    switch(contador){
+                        case 1:
+                            variable=6;
+                        break;
+                        case 2:
+                            variable=66;
+                            break;
+                        case 3:
+                            variable=666
+                            break;
+                            case 4:
+                                variable=6666;
+                            break;
+                            case 5 :
+                            variable=66666;
+                            break;
+                            case 6 :
+                            variable=666666;
+                            break;
+                            case 7 :
+                            variable=6666666;
+                            break;
+                            case 8 :
+                            variable=66666666;
+                            break;
+                            
+                    }
+                    }
+                    function asignar7(){
+                        contador++;
+                        switch(contador){
+                            case 1:
+                                variable=7;
+                            break;
+                            case 2:
+                                variable=77;
+                                break;
+                            case 3:
+                                variable=777;
+                                break;
+                                case 4:
+                                    variable=7777;
+                                break;
+                                case 5 :
+                                variable=77777;
+                                break;
+                                case 6 :
+                                variable=777777;
+                                break;
+                                case 7 :
+                                variable=7777777;
+                                break;
+                                case 8 :
+                                variable=77777777;
+                                break;
+                                default:
+                                    agregar();
+                        }
+                        }
+                        function asignar8(){
+                            contador++;
+                            switch(contador){
+                                case 1:
+                                    variable=8;
+                                break;
+                                case 2:
+                                    variable=88;
+                                    break;
+                                case 3:
+                                    variable=888;
+                                    break;
+                                    case 4:
+                                        variable=8888;
+                                    break;
+                                    case 5 :
+                                    variable=88888;
+                                    break;
+                                    case 6 :
+                                    variable=888888;
+                                    break;
+                                    case 7 :
+                                    variable=8888888;
+                                    break;
+                                    case 8 :
+                                    variable=88888888;
+                                    break;
+                                    
+                            }
+                            }
+                            function asignar9(){
+                                contador++;
+                                switch(contador){
+                                    case 1:
+                                        variable=9;
+                                    break;
+                                    case 2:
+                                        variable=99;
+                                        break;
+                                    case 3:
+                                        variable=999;
+                                        break;
+                                        case 4:
+                                            variable=9999;
+                                        break;
+                                        case 5 :
+                                        variable=99999;
+                                        break;
+                                        case 6 :
+                                        variable=999999;
+                                        break;
+                                        case 7 :
+                                        variable=9999999;
+                                        break;
+                                        case 8 :
+                                        variable=99999999;
+                                        break;
+                                    
+                                }
+                                }
+
+function asignarmas(){
+    salvar=variable;
+    operacion=1;
+    contador=0;
+    y=1;
+}
+function asignarmenos(){
+    salvar=variable;
+    operacion=2;
+    contador=0;
+    y=1;
+}
+
+function asignarpor(){
+    salvar=variable;
+    operacion=3;
+    contador=0;
+    y=1;
+}
+function asignardivicion(){
+    salvar=variable;
+    operacion=4;
+    contador=0;
+    y=1;
+}
+function asignarporcentaje(){
+    salvar=variable;
+    operacion=5;
+    contador=0;
+    y=1;
+}
+function asignarmasmenos(){
+    salvar=variable;
+    operacion=6;
+    contador=0;
+    y=1;
+}
+
+
+
+function resultado(){
+    salvar1=variable;
+    contador=0;
+
+    switch (operacion) { 
+        case 1: 
+           return sumar();
+           
+        case 2:
+           return restar(); 
+    
+        case 3: 
+            return multiplicar();
+        case 4:
+            return dividir();
+        case 5:
+             porcentaje();
+        case 6: 
+            return invertir();
+          
+           
+        default: 
+         
+ }
+ operacion=0;   
+
+}
+function sumar(a){
+    a= salvar+variable;
+    variable=a;
+    return a ;
+    
+}
+function restar(b){
+    b= (salvar)-(variable);
+    variable=b;
+    return b ;
+    
+    
+}
+function multiplicar(c){
+    c= salvar*variable;
+    variable=c;
+    return c ;
+     
+}
+function dividir(d){
+    d= salvar/variable;
+    variable=d;
+    return d ;
+     
+}
+function porcentaje(e){
+    e= (100/variable)*salvar;
+    variable=e;
+    return e  ;
+     
+}
+
